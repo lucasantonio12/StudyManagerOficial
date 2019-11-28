c4471390ced8c294531ec1599ae5a9fb78a289f9
lucas <lucas-antonio2011@hotmail.com>
2019-11-28 20:59:46 -0300

    metodo salvar criado.

--- app/src/main/java/com/example/studymanageroficial/viewlogin/CadastroLogin.kt
+++ app/src/main/java/com/example/studymanageroficial/viewlogin/CadastroLogin.kt
@@ -2,12 +2,28 @@ package com.example.studymanageroficial.viewlogin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.room.Room
 import com.example.studymanageroficial.R
+import com.example.studymanageroficial.conect.Conexao
+import com.example.studymanageroficial.modelo.Usuario
+import kotlinx.android.synthetic.main.activity_cadastro_login.*
+import kotlin.math.log
 
 class CadastroLogin : AppCompatActivity() {
-
+    val conexao: Conexao by lazy{
+        Room.databaseBuilder(this, Conexao::class.java,"DBstudyManager")
+            .allowMainThreadQueries().build()
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cadastro_login)
+
+        salvarUsuario.setOnClickListener {
+            var usuario = Usuario(nomeTXT.text.toString(),dataTXT.text.toString(),loginTXT.text.toString(),senhaTXT.text.toString())
+            conexao.UsuarioDAO().inserir(usuario)
+
+            conexao.UsuarioDAO().listAll().forEach{Log.i("Usuario",it.toString())}
+        }
     }
 }
