144e030238eecbe777653f294a0825c100438d55
lucas <lucas-antonio2011@hotmail.com>
2019-12-02 16:17:44 -0300

    - DatePicker funcionando

--- app/src/main/java/com/example/studymanageroficial/viewlogin/CadastroLogin.kt
+++ app/src/main/java/com/example/studymanageroficial/viewlogin/CadastroLogin.kt
@@ -1,23 +1,30 @@
 package com.example.studymanageroficial.viewlogin
 
+import android.app.DatePickerDialog
+import android.app.DatePickerDialog.OnDateSetListener
+import android.app.Dialog
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.room.Room
 import com.example.studymanageroficial.R
 import com.example.studymanageroficial.conect.Conexao
 import com.example.studymanageroficial.modelo.Usuario
 import kotlinx.android.synthetic.main.activity_cadastro_login.*
 import java.util.*
-import kotlin.math.log
+
 
 class CadastroLogin : AppCompatActivity() {
     val conexao: Conexao by lazy{
         Room.databaseBuilder(this, Conexao::class.java,"DBstudyManager")
             .allowMainThreadQueries().build()
     }
+
+    var DATE_DIALOG_ID = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cadastro_login)
@@ -38,6 +45,9 @@ class CadastroLogin : AppCompatActivity() {
                 Toast.makeText(this,"Existe campos vazios",Toast.LENGTH_LONG).show()
         }
 
+       floatingButtonDate.setOnClickListener {
+            onClick(it)
+       }
     }
 
     fun camposUsuarioVazio():Boolean{
@@ -49,6 +59,38 @@ class CadastroLogin : AppCompatActivity() {
         dataTXT.setText("")
         loginTXT.setText("")
         senhaTXT.setText("")
+    }
+
+
+    override fun onCreateDialog(id: Int): Dialog? {
+        val calendario = Calendar.getInstance()
+        val ano = calendario[Calendar.YEAR]
+        val mes = calendario[Calendar.MONTH]
+        val dia = calendario[Calendar.DAY_OF_MONTH]
+        when (id) {
+            DATE_DIALOG_ID -> return DatePickerDialog(
+                this, mDateSetListener, ano, mes,
+                dia
+            )
+        }
+        return null
+    }
+
+    private val mDateSetListener =
+        OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+            val data = (dayOfMonth.toString() + " /"
+                    + (monthOfYear + 1).toString() + " /" + year.toString())
+            Toast.makeText(
+                this,
+                "DATA = $data", Toast.LENGTH_SHORT
+            )
+                .show()
+
+            dataTXT.setText(data)
+        }
+
+    fun onClick(v: View) {
+        if (v === floatingButtonDate) showDialog(DATE_DIALOG_ID)
     }
 
 }
